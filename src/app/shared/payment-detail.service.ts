8883adb5697dcc80caf97caa048845fa1610fdba
Santiago Cepeda <santyagor@outlook.com>
2021-01-04 21:16:07 -0500

    Final app with fix

--- src/app/shared/payment-detail.service.ts
+++ src/app/shared/payment-detail.service.ts
@@ -7,23 +7,27 @@ import {  HttpClient } from '@angular/common/http';
 })
 export class PaymentDetailService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
   readonly baseURL = 'http://localhost:49248/api/PaymentDetail';
   formData: PaymentDetail = new PaymentDetail();
   list: PaymentDetail[];
 
+  // tslint:disable-next-line: typedef
   postPaymentDetail(){
-    return this.http.post(this.baseURL,this.formData);
+    return this.http.post(this.baseURL, this.formData);
   }
 
+  // tslint:disable-next-line: typedef
   putPaymentDetail(){
-    return this.http.put(`${this.baseURL}/${this.formData.paymentDetailId}`,this.formData);
+    return this.http.put(`${this.baseURL}/${this.formData.paymentDetailId}`, this.formData);
   }
+  // tslint:disable-next-line: typedef
   deletePaymentDetail(id: number){
     return this.http.delete(`${this.baseURL}/${id}`);
   }
 
+  // tslint:disable-next-line: typedef
   refreshList(){
     this.http.get(this.baseURL)
     .toPromise()
